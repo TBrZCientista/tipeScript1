89dab0f41fedfb0b84f12becccc255c25382dbea
Felipe <felipefreitasdaschagas@hotmail.com>
2022-05-27 05:39:47 -0300

    iniciando a imlantação da view da negociação

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,23 @@
+export class NegociacoeView {
+    template(): string {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>
+                            DATA
+                        </th>
+                        <th>
+                            QUANTIDADE
+                        </th>
+                        <th>
+                            VALOR
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        `
+    }
+}
